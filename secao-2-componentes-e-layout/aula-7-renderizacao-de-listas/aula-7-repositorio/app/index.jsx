56fec5934ea85daf009bcb827efa4e78cc864176
Raphael Barreto <raphael.boliveira@gmail.com>
2026-02-04 10:33:44 -0300

    Upload aula-7

--- secao-2-componentes-e-layout/aula-7-renderizacao-de-listas/aula-7-repositorio/app/index.jsx
+++ secao-2-componentes-e-layout/aula-7-renderizacao-de-listas/aula-7-repositorio/app/index.jsx
@@ -1,15 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import logo from "../assets/images/check.png";
+import { colors } from "../styles/colors";
+import { FlatList } from 'react-native';
+import Task from '../components/task';
 
-export default function Home() {
+export default function RootLayout() {
+  const tasks = [
+    { id: 1, completed: true, text: "Fazer café" },
+    { id: 2, completed: false, text: "Estudar React Native" },
+    { id: 3, completed: false, text: "Academia" }
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>SENAI / MOBILE</Text>
-      <Text>Projeto limpo - iniciado com sucesso!</Text>
+    <View style={estilo.mainContainer}>
+      <View style={estilo.rowContainer}>
+        <Image source={logo} style={estilo.image} />
+        <Text style={estilo.title}>
+          Minhas Tarefas
+        </Text>
+      </View>
+      <View style={estilo.rowContainer}>
+        <TextInput style={estilo.input} placeholder="Adicione um item" />
+        <Pressable
+          onPress={() => alert("Oi")}
+          style={({ pressed }) => [
+            estilo.button,
+            {
+              backgroundColor: pressed
+                ? "blue"
+                : colors.primary
+            }
+          ]}
+        >
+          <Text style={estilo.buttonText}>+</Text>
+        </Pressable>
+      </View>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Task text={item.text} />}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-  title: { fontSize: 24, fontWeight: 'bold', color: '#008542' }
+const estilo = StyleSheet.create({
+  image: {
+    width: 30,
+    height: 30
+  },
+  title: {
+    fontSize: 30,        
+    fontFamily: "Calibri",  
+    fontWeight: 600,     
+    marginLeft: 10
+  },
+  rowContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    marginBottom: 20
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 16,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 20,
+    flexGrow: 1
+  },
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: colors.primary,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 24,
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  mainContainer:{
+    padding: 20
+  }
+
 });
+
+
+
+
+
---
+++ secao-2-componentes-e-layout/aula-7-renderizacao-de-listas/aula-7-repositorio/components/task.jsx
@@ -0,0 +1,31 @@
+import { Ionicons } from "@expo/vector-icons"
+import { Pressable, StyleSheet, Text, View } from "react-native"
+import { colors } from "../styles/colors"
+
+export default function Task({ text }) {
+    return (
+        <View style={style.rowContainer}>
+            <Pressable>
+                <Ionicons
+                    name="checkmark-circle"
+                    size={32}
+                    color={colors.primary}
+                />
+            </Pressable>
+            <Text>{text}</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    rowContainer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+        marginBottom: 10
+    }
+})
+
+
+
